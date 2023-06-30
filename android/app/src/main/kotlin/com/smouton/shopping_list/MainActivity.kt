8b60da1011df4852ff4e6765cdb82a77e6fab05a
Steffan Mouton <moutonsteffan@gmail.com>
2023-06-29 19:07:12 -0500

    Created a simple shopping list app

---
+++ android/app/src/main/kotlin/com/smouton/shopping_list/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.smouton.shopping_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/grocery_list.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/widgets/new_item.dart';
+
+import '../models/grocery_item.dart';
+
+class GroceryList extends StatefulWidget {
+  const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  final List<GroceryItem> _groceryItems = [];
+
+  void _addItem() async {
+    final newItem = await Navigator.of(context).push<GroceryItem>(
+      MaterialPageRoute(
+        builder: (ctx) => const NewItem(),
+      ),
+    );
+    if (newItem == null) {
+      return;
+    }
+
+    setState(() {
+      _groceryItems.add(newItem);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Groceries'),
+        actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
+      ),
+      body: _groceryItems.isNotEmpty
+          ? ListView.builder(
+              itemCount: _groceryItems.length,
+              itemBuilder: (ctx, index) => Dismissible(
+                key: ValueKey(_groceryItems[index].id),
+                onDismissed: (direction) {
+                  setState(() {
+                    _groceryItems.removeAt(index);
+                  });
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text("Item dismissed")));
+                },
+                child: ListTile(
+                  title: Text(_groceryItems[index].name),
+                  leading: Container(
+                    width: 24,
+                    height: 24,
+                    color: _groceryItems[index].category.color,
+                  ),
+                  trailing: Text(
+                    _groceryItems[index].quantity.toString(),
+                  ),
+                ),
+              ),
+            )
+          : Center(
+              child: Text(
+                'Add some items to your grocery list!',
+                style: Theme.of(context).textTheme.headlineSmall,
+              ),
+            ),
+    );
+  }
+}
